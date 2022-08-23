3c59fab85df965faf5c0dc8b631cd3e3c6faf06a
rudy <doomdexter07@gmail.com>
2022-08-23 23:36:38 +0530

    added last model file

--- valostore/models.py
+++ valostore/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image=models.ImageField()
+    server = models.CharField(max_length=10)
+    slug = models.SlugField(default="-")
+    last_online = models.DateTimeField(auto_now=True)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email_id = models.EmailField()
+    phone = models.IntegerField()
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
